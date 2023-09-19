cdb12c29b38d550ef32239b6c29b73cf9e771552
Nuriya Akhmedshina <nyriya36@gmail.com>
2023-09-19 13:04:11 +0500

    todoitemdao updated

--- app/src/main/java/com/selva/todolist/TodoItemDao.kt
+++ app/src/main/java/com/selva/todolist/TodoItemDao.kt
@@ -1,4 +1,15 @@
 package com.selva.todolist
 
-class TodoItemDao {
+import androidx.room.Dao
+import androidx.room.Query
+import com.selva.todolist.models.TodoItem
+import java.util.concurrent.Flow
+
+@Dao
+interface TodoItemDao {
+    @Query("SELECT * FROM task_item_table ORDER BY id ASK")
+    fun allTodoItems(): Flow<TodoItem>
+
+    suspend fun insertTodoItem(todoItem: TodoItem)
+    suspend fun updateTodoItem(todoItem: TodoItem)
 }
