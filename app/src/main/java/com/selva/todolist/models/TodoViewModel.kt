40e3c32004465bb4cfa56d066cd402a063d15feb
Nuriya Akhmedshina <nyriya36@gmail.com>
2023-09-19 16:38:59 +0500

    todoviewmodel modified

--- app/src/main/java/com/selva/todolist/models/TodoViewModel.kt
+++ app/src/main/java/com/selva/todolist/models/TodoViewModel.kt
@@ -1,33 +1,26 @@
 package com.selva.todolist.models
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import com.selva.todolist.TodoItemRepository
+import kotlinx.coroutines.launch
 import java.time.LocalDate
 
 class TodoViewModel(private val repository: TodoItemRepository): ViewModel() {
-    var todoItems: LiveData<List<TodoItem>> = repository.all
+    var todoItems: LiveData<List<TodoItem>> = repository.allTodoItems.asLiveData()
 
-    init {
-        todoItems.value = mutableListOf()
+    fun addToDoItem(newTodoItem: TodoItem) = viewModelScope.launch{
+        repository.insertTodoItem(newTodoItem)
     }
 
-    fun addToDoItem(newTodoItem: TodoItem){
-        val list = todoItems.value
-        list!!.add(newTodoItem)
-        todoItems.postValue(list)
+    fun updateToDoItem(todoItem: TodoItem) = viewModelScope.launch{
+        repository.updateTodoItem(todoItem)
     }
 
-//    fun updateToDoItem(id:String, text:String, flag:Boolean){
-//        val list = todoItems.value
-//        todoItems.postValue(list)
-//    }
-//
-//    fun setCompleted(todoItem: TodoItem){
-//        val list = todoItems.value
-//        todoItems.postValue(list)
-//    }
+    fun setCompleted(todoItem: TodoItem) = viewModelScope.launch{
+        repository.updateTodoItem(todoItem)
+    }
+}
 
+class TodoItemModelFactory(private val repository: TodoItemRepository): ViewModelProvider.Factory{
 
 }
